48e42756f74d513b7191a5081485468f6173e9d7
tgdriro5 <ronald.rink@d-fens.net>
2014-11-10 08:51:32 +0100

    2014-11-10; rrink; ADD: ConvertFrom-CmdletHelp

---
+++ ConvertFrom-CmdletHelp.ps1
@@ -0,0 +1,45 @@
+Function ConvertFrom-CmdletHelp {
+	[CmdletBinding(
+		HelpURI='http://dfch.biz/PS/System/Utilities/ConvertFrom-CmdletHelp/'
+    )]
+	PARAM (
+	$CommandName = $MyInvocation.MyCommand.Name
+	,
+	[ValidateSet('default', 'json', 'json-pretty', 'xml', 'xml-pretty', 'markdown', 'gridview')]
+	[Parameter(Mandatory = $false)]
+	[alias("ReturnFormat")]
+	[string] $As = 'default'
+	)
+
+	# Default test variable for checking function response codes.
+	[Boolean] $fReturn = $false;
+	# Return values are always and only returned via OutputParameter.
+	$OutputParameter = $null;
+	
+	[string] $fn = $MyInvocation.MyCommand.Name;
+
+	$helpFormatted = @();
+	foreach($line in ((help $CommandName -Full))) { 
+	  if( ![string]::IsNullOrWhiteSpace($line) -and $line -ceq $line.ToUpper() ) {
+		$line = "{0}`r`n" -f $line; 
+	  } # if
+	  $helpFormatted += $line; 
+	} # foreach
+
+	$r = $helpFormatted;
+	switch($As) {
+	'xml' { $OutputParameter = (ConvertTo-Xml -InputObject $r).OuterXml; }
+	'xml-pretty' { $OutputParameter = Format-Xml -String (ConvertTo-Xml -InputObject $r).OuterXml; }
+	'json' { $OutputParameter = ConvertTo-Json -InputObject $r -Compress; }
+	'json-pretty' { $OutputParameter = ConvertTo-Json -InputObject $r; }
+	'gridview' { $r | Out-GridView -Title $CommandName; }
+	Default { $OutputParameter = $r; }
+	} # switch
+	$fReturn = $true;
+	return $OutputParameter
+} # ConvertFrom-CmdletHelp
+if($MyInvocation.PSScriptRoot) { Export-ModuleMember -Function ConvertFrom-CmdletHelp; }
+
+<#
+2014-11-10; rrink; ADD: ConvertFrom-CmdletHelp
+#>
