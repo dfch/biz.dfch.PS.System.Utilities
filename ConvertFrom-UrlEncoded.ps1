7df6325268396a4b0b5f5f1d23f77f9183796f27
Ronald Rink <ronald.rink@d-fens.net>
2014-11-29 08:58:30 +0000

    2014-11-29; rrink; CHG: fix pipeline behaviour

--- ConvertFrom-UrlEncoded.ps1
+++ ConvertFrom-UrlEncoded.ps1
@@ -1,31 +1,44 @@
 Function ConvertFrom-UrlEncoded {
-	[CmdletBinding(
-		HelpURI='http://dfch.biz/biz/dfch/PS/System/Utilities/ConvertFrom-UrlEncoded/'
-    )]
-	[OutputType([string])]
-	PARAM(
-	    [Parameter(ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true, Mandatory = $false, Position=0)]
-	    [string]
-	    $InputObject
-	)
-	BEGIN {
+[CmdletBinding(
+	HelpURI='http://dfch.biz/biz/dfch/PS/System/Utilities/ConvertFrom-UrlEncoded/'
+)]
+[OutputType([string])]
+
+PARAM
+(
+	[Parameter(Mandatory = $true, ValueFromPipeline = $true, Position = 0)]
+	$InputObject
+)
+
+BEGIN 
+{
 	$datBegin = [datetime]::Now;
 	[string] $fn = $MyInvocation.MyCommand.Name;
-	Log-Debug -fn $fn -msg ("CALL. Length: '{0}'; NonAlpha: '{1}'" -f $Length, $NonAlpha) -fac 1;
-	} # BEGIN
-	PROCESS {
-	$fReturn = $false;
 	$OutputParameter = $null;
+	Log-Debug -fn $fn -msg ("CALL. InputObject.Count: '{0}'" -f $InputObject.Count) -fac 1;
+}
 
-	$OutputParameter = [System.Web.HttpUtility]::UrlDecode($InputObject);
-	return $OutputParameter;
-	} # PROCESS
-	END {
+PROCESS 
+{
+	foreach($Object in $InputObject) 
+	{
+		$fReturn = $false;
+		$OutputParameter = $null;
+
+		$OutputParameter = [System.Web.HttpUtility]::UrlDecode($InputObject);
+		$OutputParameter;
+	}
+	$fReturn = $true;
+}
+
+END 
+{
 	$datEnd = [datetime]::Now;
 	Log-Debug -fn $fn -msg ("RET. fReturn: [{0}]. Execution time: [{1}]ms. Started: [{2}]." -f $fReturn, ($datEnd - $datBegin).TotalMilliseconds, $datBegin.ToString('yyyy-MM-dd HH:mm:ss.fffzzz')) -fac 2;
-	} # END
-} # ConvertFrom-UrlEncoded
-Export-ModuleMember -Function ConvertFrom-UrlEncoded;
+}
+
+} # function
+if($MyInvocation.ScriptName) { Export-ModuleMember -Function ConvertFrom-UrlEncoded; } 
 
 
 # SIG # Begin signature block
